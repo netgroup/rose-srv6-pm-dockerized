d38a3b2a042dcbb0e210b9ed6ca64bf9b92477b5
Francesco Lombardo <zvfvrv@users.noreply.github.com>
2020-05-15 20:08:48 +0200

    update some repo names

--- scripts/build_deploy_tutorial.sh
+++ scripts/build_deploy_tutorial.sh
@@ -2,13 +2,13 @@
 
 # setup_frr.sh
 
-WORKSPACE="$HOME/Workspace"
+WORKSPACE="$HOME/workspace"
 
-FLASK_APP="$WORKSPACE/srv6-pm-flask"
+FLASK_APP="$WORKSPACE/rose-srv6-pm-flask"
 
 DASHBOARD_REPO="$WORKSPACE/rose-dashboard"
 
-DOCKERIZED_REPO="$WORKSPACE/srv6-pm-dockerized"
+DOCKERIZED_REPO="$WORKSPACE/rose-srv6-pm-dockerized"
 
 cd $FLASK_APP
 
